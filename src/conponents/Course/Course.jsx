dbec482bc685d4bdaac195c5994176639db7cde3
moniru005 <moniru005@gmail.com>
2023-09-15 19:45:35 +0600

    created fake data, using useEffect and useState for update & fetch courses data and updated css

--- src/conponents/Course/Course.jsx
+++ src/conponents/Course/Course.jsx
@@ -1,15 +1,19 @@
-import image from '../../../public/image/gd.jpg'
-const Course = () => {
+const Course = ({ course }) => {
+    const { title, details, image, price, credit_hour } = course;
     return (
         <div className="course-container flex justify-center">
             <div className="course-card w-72 h-[353px] p-3 rounded-lg bg-white">
                 <img src={image} alt="" />
-                <h1>Title</h1>
-                <p><small>Lorem ipsum dolor sit amet.</small></p>
+                <h1 className="my-2 font-semibold">{title}</h1>
+                <p className="text-gray-500 h-[72px]">
+                    <small>{details}</small>
+                </p>
                 <div className="text-gray-500 flex justify-between items-center my-3 text-base">
-                    <p>$ <span>Price: 200</span></p>
+                    <p ><span className='text-lg font-semibold'>$ </span>
+                        Price: {price}
+                    </p>
                     <p>Icon</p>
-                    <p>Credit: 2hr</p>
+                    <p>Credit: {credit_hour}hr</p>
                 </div>
                 <button className="px-4 py-1 bg-blue-700 text-white rounded-md flex mx-auto w-full">
                     <span className="flex mx-auto">Select</span>
--- src/conponents/Courses/Courses.jsx
+++ src/conponents/Courses/Courses.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import Cart from "../Cart/Cart";
 import Course from "../Course/Course";
+import { useEffect } from "react";
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        fetch("data.json")
+            .then((res) => res.json())
+            .then((data) => setCourses(data));
+    }, []);
+
     return (
-        <div>
-            <h1>Course Registration</h1>
-            <Course></Course>
+        <div className="container mx-auto">
+            <div className="text-center my-6">
+                <h1 className="text-3xl font-bold">Course Registration</h1>
+            </div>
+            <div className="flex  justify-center gap-2">
+                <div className="flex flex-wrap justify-center w-3/4 gap-4 ml-4">
+                    {courses.map((course) => (
+                        <Course
+                            key={course.id}
+                            course={course}
+                        ></Course>
+                    ))}
+                </div>
+                <div className="w-1/4 flex flex-col items-center mr-4">
+                    <Cart></Cart>
+                </div>
+            </div>
         </div>
     );
 };
